f1ba17fd94ec4ce387e11a888eccf1e85925239f
MarjanMatic <marjanmatic.dev@gmail.com>
2025-11-08 20:12:40 +0100

    add notify and find new posts functions

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ def main():
 
     if os.path.isfile("./last_check.json") == False:
         create_last_check_file(driver, config["course_ids"])
+    else:
+        with open("last_check.json", "r") as f:
+            last_check = json.load(f)
+        last_check = notify_new_courses_posts(driver, last_check)
+        with open("last_check.json", "w") as f:
+            json.dump(last_check, f)
 
     time.sleep(1)
     driver.quit()
@@ -67,10 +73,10 @@ def create_last_check_file(driver, course_ids):
     with open("last_check.json", "w") as f:
         json.dump(checkJson, f)
 
-def has_search_results(driver):
+def has_search_results(driver) -> bool:
     page_content = driver.find_element(By.ID, "page-content")
     result = page_content.find_element(By.TAG_NAME, "h3").text
-    return result.find(":") == -1
+    return result.find(":") != -1
 
 def find_last_courses_posts(driver, course_ids):
     checkJson = {
@@ -90,6 +96,44 @@ def find_last_courses_posts(driver, course_ids):
         time.sleep(1)
 
     return checkJson
+
+def notify_new_courses_posts(driver, last_check):
+    new_timestamp = int(time.time())
+    for id in last_check["courses"]:
+        driver.get(f"https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id={id}&datefrom={last_check["timestamp"]}")
+
+        if has_search_results(driver):
+            articles = driver.find_elements(By.TAG_NAME, "article")
+            new_href = notify_new_posts(articles, last_check["courses"][id])
+            last_check["courses"][id] = new_href
+        
+        time.sleep(1)
+
+    last_check["timestamp"] = new_timestamp
+    print(last_check)
+    return last_check
+
+def notify_new_posts(articles, last_href):
+    article_stack = find_which_posts_are_new(articles, last_href)
+    new_href = last_href
+    while len(article_stack) > 0:
+        (article, new_href) = article_stack.pop()
+        print(article.text)
+    
+    return new_href
+
+
+def find_which_posts_are_new(articles, last_href):
+    article_stack = []
+    for article in articles:
+        permalink = article.find_element(By.PARTIAL_LINK_TEXT, "Permalink")
+        href = permalink.get_attribute("href")
+        if href == last_href:
+            break
+
+        article_stack.append((article, href))
+    
+    return article_stack
 
 load_dotenv()
 main()
